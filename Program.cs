ba01bcc40daa2745742170eeb48a3f0cc6591c7d
st10462344-lungi <ntshangalungi14@gmail.com>
2026-08-14 11:01:37 +0200

    icommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a C# console application.");
+            Console.WriteLine("Running on .NET 10.0 with C# 14.0 features.");
+        }
+    }
+}
