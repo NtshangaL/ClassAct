3769f5b3fc76cf88a4b79d5170d998b929259e27
st10462344-lungi <ntshangalungi14@gmail.com>
2026-08-14 11:15:25 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("This is a C# console application.");
+            Console.WriteLine("This is a C# console app.");
             Console.WriteLine("Running on .NET 10.0 with C# 14.0 features.");
         }
     }
